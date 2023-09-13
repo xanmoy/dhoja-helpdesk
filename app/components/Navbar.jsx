0b2487ba5aee104f20e46303edb0c3204b4fe19e
Xanmoy <xanmoy@outlook.com>
2023-09-14 01:44:13 +0530

    first commit

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import Logo from './dojo-logo.png'
+export default function Navbar() {
+  return (
+    <nav>
+      <Image 
+      src={Logo}
+      alt='Dhoja Helpdesk Logo'
+      width={70}
+      quality={100}
+      placeholder='blur'
+      />
+          <h1>Dhoja Helpdesk</h1>
+          <Link href="/">Dashboard</Link>
+          <Link href="/tickets">Tickets</Link>
+        </nav>
+  )
+}
---
+++ app/tickets/page.jsx
@@ -0,0 +1,15 @@
+// rfc 
+import TicketList from "./TicketList"
+export default function Tickets() {
+    return (
+      <main>
+          <nav>
+            <div>
+              <h2>Tickets</h2>
+              <p><small>Currently open tickets</small></p>
+            </div>
+          </nav>
+          <TicketList/>
+      </main>
+    )
+  }
